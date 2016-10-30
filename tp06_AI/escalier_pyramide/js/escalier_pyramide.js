5cda54e8fdc0092800e82255514eb35b359ea4c1
DrEtRhI <thierryeric@free.fr>
2016-10-30 17:17:03 +0100

    Fin TP6

--- tp06_AI/escalier_pyramide/js/escalier_pyramide.js
+++ tp06_AI/escalier_pyramide/js/escalier_pyramide.js
@@ -28,9 +28,46 @@ function dessinerEscalierDroit() {
 
 }
 
+function dessinerEscalierPyramide(){
+    var canvas = document.getElementById("dessinEscalier");
+    var ctx = canvas.getContext("2d");
+    var nbM = document.getElementById("nombremarche").value;
+    var H = document.getElementById("hauteur").value;
+    var colstroke = document.getElementById("colorstroke").value;
+    var colfill = document.getElementById("colorfill").value;
+    var posY, posX, L, i;
+    
+    i = 0;
+    posX = (canvas.width /2) - H;
+    posY = 1;
+    L = H * 2;
+    
+    while (i < nbM){
+        ctx.beginPath();
+        ctx.strokeStyle = colstroke;
+        ctx.fillStyle = colfill;
+        ctx.rect(posX, posY, L, H);
+        ctx.stroke();
+        ctx.fill();
+        
+        posX = parseInt(posX) - parseInt(H);
+        posY = parseInt(posY) + parseInt(H);
+        L = parseInt(L) + (parseInt(H) * 2);
+        i = i + 1;
+    }
+}
+
 function effacerCanvas() {
    var canvas = document.getElementById("dessinEscalier");
    var ctx = canvas.getContext("2d");
    
    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function choixDessin(){
+    if (document.getElementById("escdroit").checked){
+        dessinerEscalierDroit();
+    }else{
+        dessinerEscalierPyramide();
+    }
 }
